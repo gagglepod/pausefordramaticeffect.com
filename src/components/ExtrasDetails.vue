f9422cd13527060fc66a4d0909550d43bbe0c34e
gagglepod <gagglepod@gmail.com>
2025-02-07 14:24:37 -0600

    Finished up Extras and Episodes

--- src/components/ExtrasDetails.vue
+++ src/components/ExtrasDetails.vue
@@ -6,7 +6,7 @@
         <div class="extras-detail__content">
           <h2 class="extras-detail__heading">Extras</h2>
           <p class="extras-detail__info">
-            Here are the plot synopsis for all 10 parts of Danger Sold Separately:
+            Learn what went into making a daily audio drama!
           </p>
         </div>
       </article>
@@ -21,8 +21,9 @@
             <a href="https://napodpomo.org/" target="_blank"
               >2019 NaPodPoMo (National Podcast Post Month) Challenge (Produce 30 Episodes
               in 30 Days)</a
-            >. At the end of the challenge, Season 1 of PAUSE included 17 Script:
-            Chapters, 13 Behind the PAUSES "Making Of" episodes, and 2 trailers.
+            >. At the end of the challenge, Season 1 of PAUSE included 17 chapters, 13
+            Behind the PAUSES "Making Of" episodes, and 2 trailers, for a total of 32
+            episodes.
           </p>
         </div>
       </article>
@@ -37,7 +38,7 @@
             <a href="https://noteasilysquished.com" target="_blank">Not Easily Squished</a
             >. <em>Between the Pauses</em> is a mini-documentary created from my weekly
             production journal that explores all the lessons I learned while making my
-            first audio drama.
+            first audio drama:
           </p>
           <ul>
             <li>
@@ -113,79 +114,180 @@
         </div>
       </article>
 
-      <!-- extras-detail #2 -->
+      <!-- extras-detail #3 -->
       <article class="extras-detail">
         <div class="extras-detail__content">
-          <h3 class="extras-detail__subheading">Production Scripts</h3>
+          <h3 class="extras-detail__subheading">Writing Process</h3>
           <p class="extras-detail__info">
             The story of PAUSE for Dramatic Effect started as a pre-selected, one-word
             writing prompt challenge called "Inktober" in October 2019. Each word in the
-            prompt inspired the title and script for what would become an episode. Of the
-            30 words, I chose 17 to become Script: Chapters by merging several Script:
-            Chapters together. Each episode script was written the day before (and
-            sometimes day-of) recording an episode with few re-recording or script edits.
-            Whatever I wrote became the dialogue of that episode.
+            prompt inspired the title and script for what would become an episode.
           </p>
-          [PDF]
+          <p class="extras-detail__info">
+            The 30 prompt words I used to write a part of the story each day in October, I
+            combine several of them to form 17 chapters of a complete season. Then I took
+            those 17 chapters and crafted 17 audio drama scripts, some written the day
+            before (and sometimes day-of) recording that episode. With no time for
+            re-recording or script edits, whatever I wrote became the dialogue, sound
+            design, and plot for that episode.
+          </p>
+          <p class="extras-detail__info">
+            The end result was a "pantsers" dream come true, complete with epic drama,
+            cringy moments, and a lot of happy accidents. I wouldn't trade the experience
+            for anything!
+          </p>
+        </div>
+      </article>
+
+      <!-- extras-detail #4 -->
+      <article class="extras-detail">
+        <div class="extras-detail__content">
+          <h3 class="extras-detail__subheading">Final Scripts</h3>
+          <p class="extras-detail__info">
+            Here are the final 17 scripts used to create PAUSE for Dramatic Effect Season
+            One: False Awakenings:
+          </p>
           <ul>
             <li>
               <a
-                href="https://storage.googleapis.com/storage.oncetold.net/80000001/20800001/pause-ch.01-ring-v3-10272019.pdf"
+                href="https://storage.googleapis.com/storage.oncetold.net/80000001/20800001/pause-ch.01-ring-final-02072025.pdf"
                 target="_blank"
                 >Script: Chapter 1 - Ring [PDF]</a
               >
             </li>
             <li>
-              <a href="#" target="_blank">Script: Chapter 2 - Mindless [PDF]</a>
+              <a
+                href="https://storage.googleapis.com/storage.oncetold.net/80000001/20800003/pause-ch.02-mindless-final-02072025.pdf"
+                target="_blank"
+                >Script: Chapter 2 - Mindless [PDF]</a
+              >
             </li>
             <li>
-              <a href="#" target="_blank">Script: Chapter 3 - Bait [PDF]</a>
+              <a
+                href="https://storage.googleapis.com/storage.oncetold.net/80000001/20800004/pause-ch.03-bait-final-02072025.pdf"
+                target="_blank"
+                >Script: Chapter 3 - Bait [PDF]</a
+              >
             </li>
             <li>
-              <a href="#" target="_blank">Script: Chapter 4 - Freeze [PDF]</a>
+              <a
+                href="https://storage.googleapis.com/storage.oncetold.net/80000001/20800005/pause-ch.04-freeze-final-02072025.pdf"
+                target="_blank"
+                >Script: Chapter 4 - Freeze [PDF]</a
+              >
             </li>
             <li>
-              <a href="#" target="_blank">Script: Chapter 5 - Build [PDF]</a>
+              <a
+                href="https://storage.googleapis.com/storage.oncetold.net/80000001/20800006/pause-ch.05-build-final-02072025.pdf"
+                target="_blank"
+                >Script: Chapter 5 - Build [PDF]</a
+              >
             </li>
             <li>
-              <a href="#" target="_blank">Script: Chapter 6 - Husky [PDF]</a>
+              <a
+                href="https://storage.googleapis.com/storage.oncetold.net/80000001/20800007/pause-ch.06-husky-final-02072025.pdf"
+                target="_blank"
+                >Script: Chapter 6 - Husky [PDF]</a
+              >
             </li>
             <li>
-              <a href="#" target="_blank">Script: Chapter 7 - Enchanted [PDF]</a>
+              <a
+                href="https://storage.googleapis.com/storage.oncetold.net/80000001/20800008/pause-ch.07-enchanted-final-02072025.pdf"
+                target="_blank"
+                >Script: Chapter 7 - Enchanted [PDF]</a
+              >
             </li>
             <li>
-              <a href="#" target="_blank">Script: Chapter 8 - Frail [PDF]</a>
+              <a
+                href="https://storage.googleapis.com/storage.oncetold.net/80000001/20800009/pause-ch.08-frail-final-02072025.pdf"
+                target="_blank"
+                >Script: Chapter 8 - Frail [PDF]</a
+              >
             </li>
             <li>
-              <a href="#" target="_blank">Script: Chapter 9 - Swing [PDF]</a>
+              <a
+                href="https://storage.googleapis.com/storage.oncetold.net/80000001/20800010/pause-ch.09-swing-final-02072025.pdf"
+                target="_blank"
+                >Script: Chapter 9 - Swing [PDF]</a
+              >
             </li>
             <li>
-              <a href="#" target="_blank">Script: Chapter 10 - Witchhunt [PDF]</a>
+              <a
+                href="https://storage.googleapis.com/storage.oncetold.net/80000001/20800011/pause-ch.10-witch-hunt-final-02072025.pdf"
+                target="_blank"
+                >Script: Chapter 10 - Witchhunt [PDF]</a
+              >
             </li>
             <li>
-              <a href="#" target="_blank">Script: Chapter 11 - Dreamtime [PDF]</a>
+              <a
+                href="https://storage.googleapis.com/storage.oncetold.net/80000001/20800012/pause-ch.11-dreamtime-final-02072025.pdf"
+                target="_blank"
+                >Script: Chapter 11 - Dreamtime [PDF]</a
+              >
             </li>
             <li>
-              <a href="#" target="_blank">Script: Chapter 12 - Strongroom [PDF]</a>
+              <a
+                href="https://storage.googleapis.com/storage.oncetold.net/80000001/20800016/pause-ch.12-strongroom-final-02072025.pdf"
+                target="_blank"
+                >Script: Chapter 12 - Strongroom [PDF]</a
+              >
             </li>
             <li>
-              <a href="#" target="_blank">Script: Chapter 13 - Wormhole [PDF]</a>
+              <a
+                href="https://storage.googleapis.com/storage.oncetold.net/80000001/20800021/pause-ch.13-wormhole-final-02072025.pdf"
+                target="_blank"
+                >Script: Chapter 13 - Wormhole [PDF]</a
+              >
             </li>
             <li>
-              <a href="#" target="_blank">Script: Chapter 14 - Groundhog [PDF]</a>
+              <a
+                href="https://storage.googleapis.com/storage.oncetold.net/80000001/20800029/pause-ch.14-groundhog-final-02072025.pdf"
+                target="_blank"
+                >Script: Chapter 14 - Groundhog [PDF]</a
+              >
             </li>
             <li>
-              <a href="#" target="_blank">Script: Chapter 15 - Shipwreck [PDF]</a>
+              <a
+                href="https://storage.googleapis.com/storage.oncetold.net/80000001/20800031/pause-ch.15-shipwreck-final-02072025.pdf"
+                target="_blank"
+                >Script: Chapter 15 - Shipwreck [PDF]</a
+              >
             </li>
             <li>
-              <a href="#" target="_blank">Script: Chapter 16 - Telltale [PDF]</a>
+              <a
+                href="https://storage.googleapis.com/storage.oncetold.net/80000001/20800035/pause-ch.16-telltale-final-02072025.pdf"
+                target="_blank"
+                >Script: Chapter 16 - Telltale [PDF]</a
+              >
             </li>
             <li>
-              <a href="#" target="_blank"
-                >Script: Chapter 17 - Jackknife (Season Finale) [PDF]</a
+              <a
+                href="https://storage.googleapis.com/storage.oncetold.net/80000001/20800038/pause-ch.17-jackknife-final-02072025.pdf"
+                target="_blank"
+                >Script: Chapter 17 - Jackknife [PDF]</a
               >
             </li>
           </ul>
+        </div>
+      </article>
+
+      <!-- extras-detail #5 -->
+      <article class="extras-detail">
+        <div class="extras-detail__content">
+          <h3 class="extras-detail__subheading">Special Thanks</h3>
+          <p class="extras-detail__info">
+            I could not have done Pause for Dramatic Effect without the help of my friends
+            and family, the awesome lessons taught during Podfest's 2019 AudioDramaCon,
+            hallway conversations with
+            <a
+              href="https://www.linkedin.com/company/wayland-productions-inc/"
+              target="_blank"
+              >KC Wayland, founder and CEO of Wayland Productions</a
+            >, and a special one-on-one interview with
+            <a href="https://www.linkedin.com/company/fable-and-folly/" target="_blank"
+              >Sean Howard, co-founder and CEO of Fable and Folly</a
+            >.
+          </p>
         </div>
       </article>
     </div>
